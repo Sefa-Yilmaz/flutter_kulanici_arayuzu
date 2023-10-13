3dc6ae1c57bd6acf9c4b91c8b69755059a2c144d
Sefa-Yilmaz <104129425+Sefa-Yilmaz@users.noreply.github.com>
2023-10-13 15:14:49 +0300

    flutter_kulanici_arayuzu

---
+++ flutter_kulanici_arayuzu/lib/friends_list.dart
@@ -0,0 +1,7 @@
+const List<Map<String, dynamic>> friends = [
+  {'image': 'assets/profile.png', 'name': 'as'},
+  {'image': 'assets/profile1.png', 'name': 'as'},
+  {'image': 'assets/profile2.png', 'name': 'as'},
+  {'image': 'assets/profile2.png', 'name': 'as'},
+  
+];
